c10878b3cdeaabdd5ef27db82e584d3871757a6e
Patbob123 <flyingbikeddd@gmail.com>
2025-03-07 11:51:32 -0500

    initial files

---
+++ event.py
@@ -0,0 +1,7 @@
+class Event:
+    #Plan: Add all attributes
+    pass
+
+class EventTree:
+    # Plan: Add attributes, just subtress [], and a method to insert an event based on what the event is
+    pass
