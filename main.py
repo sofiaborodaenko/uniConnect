5b8a3a1f5c1975015173f8b9e7681c6ae1fab9a2
Sofia Borodaenko <soph.bor06@gmail.com>
2025-03-09 14:29:34 -0400

    checking if i can push with no issues

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 print("Hello darkness my old friend")
-
+print("checking if i can push and pull")
 #Need a method to parse data
 #Import tkinter and Flask
 #(Probably optional for now) Add a scrapy to scrape off sites
